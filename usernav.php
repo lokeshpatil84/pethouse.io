22060be3420293f70a011c73cd7cca6cbac812b3
lokesh patil <lokeshpatil8484@gmail.com>
2021-12-16 11:53:01 +0530

    modified

---
+++ usernav.php
@@ -0,0 +1,28 @@
+<?php
+$res=!isset($_SESSION['unm']);
+$res1=$_SESSION['role']!="user";
+if($res || $res1)
+{
+  echo "<script>alert('Invalid user,please login first')</script>";
+  echo "<script>window.location='login.php'</script>";
+}
+?>
+<?php
+     $name=$_SESSION['unm']
+	 
+     ?>
+      <div class="searchform">
+
+      </div>
+      <div class="menu_nav">
+        <ul>
+          <li class="active"><a href="usermain.php"><span>Home</span></a></li>
+          <li><a href="useraddpost.php"><span>Add Post</span></a></li>
+
+          <li><a href='delete.php?name=<?php echo $name ?>'><span>View Post</span></a></li>
+          <li><a href="login.php"><span>Logout</span></a></li> 
+        </ul>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
